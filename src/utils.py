09bced218f57f5d8661994e6ffb5ae1f05e7c0b9
Gitanjali Thanancheyan <1330230+GitanjaliThiyagalingam@users.noreply.github.com>
2024-07-14 20:31:24 +1000

    Hyper parameter training added

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,7 @@ import dill
 
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -21,12 +22,17 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
+            gs = GridSearchCV(model, param, cv = 3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
